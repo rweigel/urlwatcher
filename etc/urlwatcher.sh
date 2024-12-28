52a86071d378b6c741d25f3e8676ad0d128be620
Bob Weigel <rweigel@gmu.edu>
2024-12-28 03:22:01 +0000

    node version

--- etc/urlwatcher.sh
+++ etc/urlwatcher.sh
@@ -1,1 +1,1 @@
-~/.nvm/versions/node/v20.11.1/bin/node --max-old-space-size=96 urlwatcher.js -p 4444 -c conf/hapi/hapi-config.json
+~/.nvm/versions/node/v20.18.1/bin/node --max-old-space-size=96 urlwatcher.js -p 4444 -c conf/hapi/hapi-config.json
